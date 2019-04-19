5e90d70c9876bb61f5756217e6c10e056b9b68fc
Daniel Georgiev <daniel.georgiev95@gmail.com>
2019-04-19 16:58:34 +0100

    ArrayList of inning combinations added

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
 /**
  * Created by dimitar on 19/04/2019.
  */
+var originalBoard;
+const humanPlayer = 'O';
+const aiPlayer = 'X';
+const winningCombos = [
+
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [6, 4, 2]
+
+];
+
+const cells = document.querySelectorAll('.cell');
+
+startGame();
+
+function startGame() {
+
+    document.querySelector(".endgame").style.display = "none";
+    originalBoard = Array.from(Array(9).keys());
+
+    for(var i = 0; i < cells.length; i++){
+
+        cells[i].innerText = '';
+        cells[i].style.removeProperty('background-color');
+
+    }
+
+}
