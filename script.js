b7812ceca8e89b1d5c9e689dfa6599ca9c31c186
Daniel Georgiev <daniel.georgiev95@gmail.com>
2019-04-20 09:50:25 +0100

    Declare a winner or a tie game functionality added

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-/**
- * Created by dimitar on 19/04/2019.
- */
 var originalBoard;
 const humanPlayer = 'O';
 const aiPlayer = 'X';
@@ -18,106 +15,122 @@ const winningCombos = [
 ];
 
 const cells = document.querySelectorAll('.cell');
-
 startGame();
 
 function startGame() {
 
     document.querySelector(".endgame").style.display = "none";
+
     originalBoard = Array.from(Array(9).keys());
 
-    for(var i = 0; i < cells.length; i++){
+    for (var i = 0; i < cells.length; i++) {
 
         cells[i].innerText = '';
         cells[i].style.removeProperty('background-color');
-        cells[i].addEventListener('click', turnClick, false)
+        cells[i].addEventListener('click', turnClick, false);
 
     }
-
 }
 
-function turnClick(square){
+function turnClick(square) {
 
-    if(typeof originalBoard[square.target.id] == 'number'){
+    if (typeof originalBoard[square.target.id] == 'number') {
 
         turn(square.target.id, humanPlayer);
 
-        if(!checkTie()) turn(bestSpot(), aiPlayer);
+        if (!checkTie()) turn(bestSpot(), aiPlayer);
 
     }
-    
 }
 
-function turn(squareId, player){
+function turn(squareId, player) {
 
     originalBoard[squareId] = player;
+
     document.getElementById(squareId).innerText = player;
+
     let gameWon = checkWin(originalBoard, player);
-    if(gameWon) gameOver(gameWon)
+
+    if (gameWon) gameOver(gameWon)
 
 }
 
-function checkWin(board, player){
+function checkWin(board, player) {
 
     let plays = board.reduce((a, e, i) =>
+
         (e === player) ? a.concat(i) : a, []);
+
     let gameWon = null;
 
-    for(let [index, win] of winningCombos.entries()){
+    for (let [index, win] of winningCombos.entries()) {
 
-        if(win.every(elem => plays.indexOf(elem) > -1)){
+        if (win.every(elem => plays.indexOf(elem) > -1)) {
 
             gameWon = {index: index, player: player};
             break;
 
         }
-
     }
 
     return gameWon;
-    
+
 }
 
-function gameOver(gameWon){
+function gameOver(gameWon) {
 
-    for(let index of winningCombos[gameWon.index]){
+    for (let index of winningCombos[gameWon.index]) {
 
-        document.getElementById(index)
-            .style.backgroundColor = gameWon.player = humanPlayer ? "blue" : "red";
+        document.getElementById(index).style.backgroundColor =
+            gameWon.player == humanPlayer ? "blue" : "red";
 
     }
 
-    for(var i = 0; i < cells.length; i++){
+    for (var i = 0; i < cells.length; i++) {
 
         cells[i].removeEventListener('click', turnClick, false);
 
     }
 
+    declareWinner(gameWon.player == humanPlayer ? "You win!" : "You lose.");
+
 }
 
-function emptySquares(){
+function declareWinner(who) {
 
-    return originalBoard.filter(s => typeof s == 'number')
+    document.querySelector(".endgame").style.display = "block";
+    document.querySelector(".endgame .text").innerText = who;
+
+}
+
+function emptySquares() {
+
+    return originalBoard.filter(s => typeof s == 'number');
 
 }
 
 function bestSpot() {
 
     return emptySquares()[0];
-    
+
 }
 
-function checkTie(){
+function checkTie() {
 
-    if(emptySquares().length == 0){
+    if (emptySquares().length == 0) {
 
-        for(var i = 0; i < cells.length; i++){
+        for (var i = 0; i < cells.length; i++) {
 
             cells[i].style.backgroundColor = "green";
             cells[i].removeEventListener('click', turnClick, false);
 
         }
 
+        declareWinner("Tie Game!")
+        return true;
+
     }
+
+    return false;
 
 }
